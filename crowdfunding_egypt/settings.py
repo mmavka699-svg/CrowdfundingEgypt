418454dac676488a7c966ca18ace4dcf5857c09e
Amr-767 <amr340792@gmail.com>
2026-08-11 15:45:38 +0300

    Prepare for deployment

--- crowdfunding_egypt/settings.py
+++ crowdfunding_egypt/settings.py
@@ -90,6 +90,9 @@ DATABASES = {
         "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
         "HOST": os.environ.get("DB_HOST", "localhost"),
         "PORT": os.environ.get("DB_PORT", "5432"),
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
 
