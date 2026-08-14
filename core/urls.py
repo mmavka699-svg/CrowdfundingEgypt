13cab34a83d2bcd725982ea97c421cab995e14a6
Amr-767 <amr340792@gmail.com>
2026-08-14 12:58:53 +0300

    added new pages

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,9 @@ app_name = "core"
 urlpatterns = [
     path("", views.home_view, name="home"),
     path("about/", views.about_view, name="about"),
+    path("verification/", views.verification_view, name="verification"),
+    path("fees/", views.fees_view, name="fees"),
+    path("refund/", views.refund_view, name="refund"),
+    path("report/", views.report_view, name="report"),
     path("api/", include("chatbot.urls")),    
 ]
